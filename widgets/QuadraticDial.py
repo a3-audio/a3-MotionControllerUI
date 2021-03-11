fcbfc0178149a8628c58e64e45f1b0ec256420bc
Patric Schmitz <bzk0711@aol.com>
2021-03-11 15:38:56 +0100

    use valueChanged instead of actionTriggered for encoder steps

--- widgets/QuadraticDial.py
+++ widgets/QuadraticDial.py
@@ -8,24 +8,19 @@ class QuadraticDial(QDial):
     def __init__(self, parent):
         super().__init__(parent)
         self.last_value = self.value()
-        self.actionTriggered.connect(self.handle_actionTriggered)
+        self.valueChanged.connect(self.handle_valueChanged)
 
-    def handle_actionTriggered(self, action):
-        # TODO: clean handling of increment/decrement calculation on keyboard input
-
-        v = self.value()
-        if(v > self.last_value):
-            if(self.last_value == self.minimum() and v == self.maximum()):
-                self.step.emit(-1)
-            else:
-                self.step.emit(1)
-        elif(v < self.last_value):
-            if(self.last_value == self.maximum() and v == self.minimum()):
-                self.step.emit(1)
-            else:
-                self.step.emit(-1)
-
-        self.last_value = v
+    def handle_valueChanged(self, value):
+        # TODO: this works fine for mouse input because all values
+        # [min, max] are received. when using keyboard input, going
+        # upwards we get (min, max] and going downward we get
+        # [min, max) for some reason, which will cause spurious steps
+        # when wrapping.
+        if(value > self.last_value):
+            self.step.emit(-1 if self.last_value == self.minimum() and value == self.maximum() else  1)
+        elif(value < self.last_value):
+            self.step.emit( 1 if self.last_value == self.maximum() and value == self.minimum() else -1)
+        self.last_value = value
 
     def sizeHint(self):
         return QSize(64, 64)
