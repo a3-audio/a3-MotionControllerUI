a136239b5a611f77c3a4b368f83106f54b92bf14
aaa <aaa>
2021-09-13 21:50:31 +0200

    set absolut path for orientation.svg; correct moc.sh

--- moc.sh
+++ moc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-python3 /home/aaa/Ambijockey/Controller_Motion/software/MotionControllerUI/moc_ui.py --serial_device /dev/ttyACM0 --ip "192.168.43.50" --control_port 9000 --encoder_base_port 1337
+/usr/bin/python3 /home/aaa/Ambijockey/Controller_Motion/software/MotionControllerUI/moc_ui.py --serial_device /dev/ttyACM0 --server_ip "192.168.43.50" --server_port 9000 --encoder_base_port 1337
