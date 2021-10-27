003baca67896b9ec947129d616d6ca6aeabb1660
Patric Schmitz <patric.schmitz@cs.rwth-aachen.de>
2021-10-27 10:24:10 +0200

    use working dir relative to script to get rid of host-specific path

--- moc.sh
+++ moc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/aaa/Ambijockey/Controller_Motion/software/MotionControllerUI
+cd $(dirname "$0")
 /usr/bin/python3 moc_ui.py --serial_device /dev/ttyACM0 --server_ip "192.168.43.50" --server_port 9000 --encoder_base_port 1337
