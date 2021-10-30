233e4b78ad153c8023392064abc7b68c5ee04788
Patric Schmitz <bzk0711@aol.com>
2021-10-30 17:32:49 +0200

    moc.sh can be called via symbolic link

--- moc.sh
+++ moc.sh
@@ -17,5 +17,5 @@
 
 # © Copyright 2021 Patric Schmitz
 
-cd $(dirname "$0")
+cd $(dirname $(readlink -f "$0"))
 /usr/bin/python3 moc_ui.py --serial_device /dev/ttyACM0 --server_ip "192.168.43.50" --server_port 9000 --encoder_base_port 1337
